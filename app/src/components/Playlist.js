08065dd0f09ec80435c4ecd0a12632b37dfb7684
Miao, Qi <327191467@qq.com>
2018-04-24 11:48:35 +0800

    Update playerlist

--- app/src/components/Playlist.js
+++ app/src/components/Playlist.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 class Playlist extends Component {
   constructor (props) {
     super(props);
-    // this.handleClick = this.handleClick.bind(this);
-}
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(id) {
+
+  }
+
 
   render() {
     return(
