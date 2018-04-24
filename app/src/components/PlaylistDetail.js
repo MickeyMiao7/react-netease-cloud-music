8f77257e93a391193f2dd6c00ec2d9134543f235
Miao, Qi <327191467@qq.com>
2018-04-24 18:15:05 +0800

    Add

--- app/src/components/PlaylistDetail.js
+++ app/src/components/PlaylistDetail.js
@@ -12,12 +12,25 @@ class PlaylistDetail extends Component {
         <div className="intro">
           <img className="cover" src="" alt=""/>
           <div className="content">
-            <span className="label-playlist">Playlist</span>
-            <span className="title">English</span>
-            <span className="icon"></span>
-            <span className=""></span>
-            <span className="title">English</span>
-          </div>
+            <section>
+              <span className="label-playlist">Playlist</span>
+              <span className="title">English</span>
+              <span className="icon"></span>
+              <span className=""></span>
+              <span className="icon"></span>
+            </section>
+            <section>
+              <img className="creator-avatar" src="" alt=""/>
+              <span className="creator-nickname">Mickey</span>
+              <span className="create-time">2018-04-24 Created</span>
+            </section>
+            <section>
+              Tag:
+            </section>
+            <section>
+              Description:
+            </section>
+         </div>
         </div>
         <div className="table">
 
