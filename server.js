1662e2bbfb6ea7878d9491eacc8651adcc01a7b7
Miao, Qi <327191467@qq.com>
2018-05-03 18:03:12 +0800

    local server

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const request = require('request')
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+	request('http://music.163.com/api/song/lyric?id=29775505', function(error, response, body) {
+		res.send(`Error: ${error}, response: ${response}, body: ${body}`)
+	})
 })
 
 app.listen(3000, function () {
