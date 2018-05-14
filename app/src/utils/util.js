d99998bda68e7ee930ea8aaf345fd5f5853f6ccd
Miao, Qi <327191467@qq.com>
2018-05-14 18:02:30 +0800

    update

---
+++ app/src/utils/util.js
@@ -0,0 +1,18 @@
+export function convertDate(timestamp) {
+  let date = new Date()
+  if (typeof timestamp === 'number' && timestamp.toString().length === 13) {
+    date = new Date(timestamp)
+  }
+  return `${date.getYear() + 1900}-${date.getMonth() + 1}-${date.getDay()}`
+}
+
+export function msToTime(duration) {
+  const total = parseInt(duration / 1000)
+  const minutes = parseInt(total / 60)
+  const seconds = parseInt(total % 60)
+  return `${formatNumber(minutes)}:${formatNumber(seconds)}`
+}
+
+export function formatNumber(number) {
+  return ('0' + number).slice(-2)
+}
